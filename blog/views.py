49b66ad5fef5bef3653e5a9e6f2405b638f8deb0
Menna Shamloul <menna.shamloul92@gmail.com>
2024-01-15 01:11:13 +0000

    add base.html in templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# create data
+
+posts = [
+    {
+        'author': 'Tarek Salah',
+        'title' : 'Speech Blog Post',
+        'content' : 'This my first post in this blog',
+        'date_posted' : '1th January 2024',
+    },
+    {
+        'author': 'Tom David',
+        'title' : 'Speech Delay Blog Post',
+        'content' : 'It is the first time to create post in speech blog',
+        'date_posted' : '3th January 2024',
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render()
+    return render(request, 'blog/about.html')
