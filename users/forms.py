994e2d05cb8fefdcc7288f9d349db9ccce7ec5f8
Menna Shamloul <menna.shamloul92@gmail.com>
2024-01-23 11:49:00 +0000

    add email in registerform

--- users/forms.py
+++ users/forms.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.author.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email
+    email = forms.EmailField()
+    
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
