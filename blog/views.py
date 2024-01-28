0c5621d2c5edacf1369ae7bcec204328b1cf90fe
Menna Shamloul <menna.shamloul92@gmail.com>
2024-01-28 21:32:28 +0000

    create post detail.htmland its url

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # create data
 
@@ -36,4 +36,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ["date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post
+    
