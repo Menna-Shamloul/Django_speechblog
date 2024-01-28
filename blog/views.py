eb28a534ec449988f083ed15ffd54297e686e157
Menna Shamloul <menna.shamloul92@gmail.com>
2024-01-28 20:58:45 +0000

    add postviewlist to view.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
 # create data
 
@@ -21,12 +22,16 @@ posts = [
 
 
 # Create your views here.
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#   context = {
+#       'posts': Post.objects.all()
+#    }
+#   return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': "About Page"})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
